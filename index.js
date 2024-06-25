cead7a2611e5b71296ca54041f3f358cbf0e9ce0
Ajut Dangol <ajud10gol@gmail.com>
2024-06-25 09:16:41 +0545

    first class

---
+++ index.js
@@ -0,0 +1,61 @@
+// varibales
+// var
+// var x = 90;
+// console.log(x);
+// var x=100;
+// console.log(x);
+
+// // let
+// let y=900;
+// console.log(y);
+//  y=100;
+// console.log(y);
+
+
+// const
+// const z=234;
+
+// console.log(z);
+
+
+
+// Data types
+// string
+// const output ="hash bro";
+// console.log(output);
+// console.log(typeof output);
+
+// number
+// const rem=134525433452452342;
+// console.log(rem);
+// console.log(typeof rem);
+
+// const bro = 12.23;
+// console.log(bro);
+// console.log(typeof bro);
+
+// undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// null
+// let nm= null;
+// console.log(nm);
+// console.log(typeof nm);
+
+// boolean
+// const hg=true;
+// console.log(hg);
+// console.log(typeof hg);
+
+// bigint
+// const result=23454543635654653n;
+// console.log(result);
+// console.log(typeof result);
+
+// symbol
+// const output=Symbol("sfsa");
+// console.log(output);
+// console.log(typeof output);
+
